a45ecdef51ffa5b5415f0660be7c340af9c81b65
Trevor Lohrbeer <trevor@fastfedora.com>
2018-04-24 01:11:47 -0400

    Add support for custom layouts
    Use the `layout` attribute for the screen data to specify a layout for a screen.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="app">
     <DebugPane :scheduler="scheduler" />
-    <DefaultLayout :programs="activePrograms" />
+    <SplitLayout v-if="layout === 'split'" :programs="activePrograms" />
+    <DefaultLayout v-else :programs="activePrograms" />
   </div>
 </template>
 
 <script>
 import DebugPane from './components/DebugPane';
 import DefaultLayout from './components/DefaultLayout';
+import SplitLayout from './components/SplitLayout';
 import Scheduler from './services/scheduler/Scheduler';
 import { loadScreenInfo } from './utils/api';
 
@@ -25,11 +27,13 @@ export default {
     return {
       scheduler: new Scheduler(),
       activePrograms: [],
+      layout: 'default',
     };
   },
   components: {
     DebugPane,
     DefaultLayout,
+    SplitLayout,
   },
   mounted() {
     loadScreenInfo(this.apiBase, this.screenId).then((response) => {
@@ -41,6 +45,10 @@ export default {
       const refreshRate = Number.parseInt(acf.screen_refresh, 10);
       const scheduleId = acf.screen_content && acf.screen_content.ID;
       const scheduleUrl = `${this.apiBase}kiosign_schedules/${scheduleId}`;
+
+      if (acf.layout) {
+        this.layout = acf.layout;
+      }
 
       if (refreshRate) {
         this.scheduler.refreshRate = refreshRate * 60;
