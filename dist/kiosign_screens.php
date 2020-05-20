8585dbf83fa3a13cbf3330fc31adf72680710b1b
digital-spaces <60891884+digital-spaces@users.noreply.github.com>
2020-05-20 16:01:22 -0400

    Added dist.

---
+++ dist/kiosign_screens.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <title>KioSign Screen-<?php echo get_the_ID()?> | <?php the_title()?></title>
+    <link href="<?php echo get_stylesheet_directory_uri(); ?>/kiosign/screen-app/manual-override.css" rel=stylesheet>
+  <link href="<?php echo get_stylesheet_directory_uri(); ?>/kiosign/screen-app/css/app.3f1e8a629b46ee0289960310524951a4.css" rel=stylesheet></head>
+  <body>
+    <div id=app data-screen-id="<?php the_ID(); ?>" data-api-base="<?php echo site_url(); ?>/wp-json/acf/v3/"></div>
+    
+  <script type=text/javascript src="<?php echo get_stylesheet_directory_uri(); ?>/kiosign/screen-app/js/manifest.05291b16757de20fd70c.js"></script><script type=text/javascript src="<?php echo get_stylesheet_directory_uri(); ?>/kiosign/screen-app/js/vendor.aa58413ca508655db25a.js"></script><script type=text/javascript src="<?php echo get_stylesheet_directory_uri(); ?>/kiosign/screen-app/js/app.98a5a9a30a82f93eb4d8.js"></script></body>
+</html>
